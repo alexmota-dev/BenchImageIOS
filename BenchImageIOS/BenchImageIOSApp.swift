28995d34ccabee16825fc6b36aecddad4531fb6f
Weslley Fernando Teixeira Chaves <weslleyfernando71609@gmail.com>
2025-09-22 19:12:13 -0300

    Initial Commit

---
+++ BenchImageIOS/BenchImageIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchImageIOSApp.swift
+//  BenchImageIOS
+//
+//  Created by Vitória Kelly on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BenchImageIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BenchImageIOSTests/BenchImageIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  BenchImageIOSTests.swift
+//  BenchImageIOSTests
+//
+//  Created by Vitória Kelly on 22/09/25.
+//
+
+import Testing
+@testable import BenchImageIOS
+
+struct BenchImageIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
